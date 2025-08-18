0423e3e1f4bbef9fa15d92db0e0c2bfc58f4388d
PedroMoreira25 <pedrohmsil92@gmail.com>
2025-08-18 12:42:49 -0300

    atualização de novos arquivos e mudanças feitas

--- main.py
+++ main.py
@@ -3,47 +3,12 @@ import time
 import os 
 from pprint import pprint
 from dotenv import load_dotenv
-from AWS.AWS import exeQuery, getQuery, idQuery
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from API_GoogleSheets import functions as fc
-from SqlQueries import vtrp_query as vtrp
+import updatesCustomers as up 
 
-load_dotenv()
-SPREADSHEET_ID = os.getenv("VTRP_SPREADSHEET_ID") 
-
-resultQuery = exeQuery('24f98c33-ae19-4f09-9601-f9ca82cf4451')
-
-start = 0
-end = 0 
-var = 1
-while start < len(resultQuery):
-    RANGE = f"NEW_atendimentos!A{var}"
-    end = end + 100
-    fc.postSheet(SPREADSHEET_ID, fc.credenciais(), RANGE, resultQuery[start:end])
-    var = var + 100
-    start = end 
-    print("\n")
-
-#TEREI QUE MUDAR O postSheet !!!!!!!!!!
-
-#fc.postSheet(SPREADSHEET_ID, fc.credenciais(), RANGE, resultQuery)
-#comentarei a linha de baixo pq estou testando o programa. Porém, quando ele estiver rodando, é só descomentar a linha de baixo
-#resultQuery = exeQuery(getQuery(idQuery(vtrp.atendimentos)))
-# fc.getSheet(SPREADSHEET_ID, fc.credenciais(), RANGE)
-
-
-
-"""
-b = 0 
-inputData = []
-while a <= len(resultQuery):
-    a = a + 500 
-    while b < len(resultQuery):
-            inputData.append(resultQuery[b])
-            b = b + 1 
-    fc.postSheet(SPREADSHEET_ID, fc.credenciais(), RANGE, inputData)    
-"""
+up.updateVtrp()
---
+++ updatesCustomers.py
@@ -0,0 +1,40 @@
+import boto3 
+import time 
+import os 
+import pandas as pd
+import csv
+from pprint import pprint 
+from dotenv import load_dotenv 
+from AWS import AWS as aws 
+from google.auth.transport.requests import Request 
+from google.oauth2.credentials import Credentials 
+from google_auth_oauthlib.flow import InstalledAppFlow 
+from googleapiclient.discovery import build 
+from googleapiclient.errors import HttpError 
+from API_GoogleSheets import functions as fc 
+from SqlQueries import queries 
+
+load_dotenv()
+
+
+def updateVtrp():
+    a = 76 
+    sql = queries.alertas.format(a=a)
+    idQuery = aws.idQuery(sql)
+    aws.DownloadResultQuery(aws.getQuery(idQuery))
+    arquivo_csv = open(f'DownloadsData/{idQuery}.csv')
+    leitor_csv = csv.reader(arquivo_csv)
+    leitor_csv = list(leitor_csv)
+    RANGE = os.getenv("ALERTAS")
+    SHEET_ID = os.getenv("11Ta_tNTE4xBBxlge6QS3N6kZ26e9ZoTYDI7EeyTo9kM")
+    fc.postSheet(SHEET_ID, fc.credenciais(), RANGE, leitor_csv)
+"""
+
+    idQuery = aws.idQuery(vtrp.atendimentos)
+    aws.DownloadResultQuery(aws.getQuery(idQuery))
+    arquivo_csv = open(f'DownloadsData/{idQuery}.csv')
+    leitor_csv = csv.reader(arquivo_csv)
+    leitor_csv = list(leitor_csv)
+    RANGE = os.getenv("VTRP_RANGE_ATENDIMENTOS")
+    fc.postSheet(SHEET_ID, fc.credenciais(), RANGE, leitor_csv)
+"""
